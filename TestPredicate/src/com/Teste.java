31f4d46a6f453cf1488280d63171c96778d3b1a9
Leandro Souza <leandrobh@outlook.com>
2013-04-23 22:51:00 -0300

    Predicate funcionando corretamente.

--- TestPredicate/src/com/Teste.java
+++ TestPredicate/src/com/Teste.java
@@ -9,34 +9,28 @@ import org.apache.commons.collections.CollectionUtils;
 public class Teste {
 
 	public void teste(){
-		
+
 		Pessoa p = new Pessoa();
 		p.setNome("Leandro");
-		
+
 		Pessoa p1 = new Pessoa();
 		p1.setNome("Gustavo");
-		
+
 		List<Pessoa> lista = new ArrayList<Pessoa>();
 		lista.add(p);
 		lista.add(p1);
-		
+
 		PredLike pred = new PredLike();
-		pred.setParametro("an");
-		
+		pred.setParametro("av");
+
 		@SuppressWarnings("unchecked")
-		Collection<Pessoa> listaRes = CollectionUtils.predicatedCollection(
-				lista, pred);
-		
-		for (Pessoa a : lista){
+		Collection<Pessoa> filteredCollection = CollectionUtils.select(lista, pred);
+
+		for (Pessoa a : filteredCollection){
 			System.out.println(a.getNome());
 		}
-		
-		for (Pessoa a : listaRes){
-			System.out.println(a.getNome());
-		}
-		
 	}
-	
+
 	public static void main(String[] args) {
 		new Teste().teste();
 	}
